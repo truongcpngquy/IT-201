3fe3a49e1074156614731fbe91d3c3e9a20efc3d
congquy <congquy010206@gmail.com>
2025-06-30 07:12:37 +0700

    bai1

--- session1/bai1.c
+++ session1/bai1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 int main()
 {
-    int n = 10;
+    int n = 10; // Ví dụ gán n để code chạy thử
+    // Độ phức tạp thời gian: O(n)
     for (int i = 0; i < n; i++)
     {
         printf("Hello\n");
